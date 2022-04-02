8ae96e3fb6d7ba3c1db23b936778f5d7a748e258
Santeri Suomi <sant.suomi@gmail.com>
2022-04-03 01:26:46 +0300

    Add default attributes in the Game.sol contract

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -5,7 +5,59 @@ pragma solidity ^0.8.13;
 import "hardhat/console.sol";
 
 contract Game {
-    constructor() {
-        console.log("THIS IS MY GAME CONTRACT. NICE.");
+    struct Attributes {
+        uint256 index;
+        string name;
+        string uri;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 damage;
+        uint256 crit;
+        uint256 heal;
+    }
+
+    Attributes[] defaultAttributes;
+
+    constructor(
+        // Default character details
+        string[] memory names,
+        string[] memory uris,
+        uint256[] memory hps,
+        uint256[] memory damages,
+        uint256[] memory crits,
+        uint256[] memory heals
+    ) {
+        require(
+            names.length == uris.length &&
+                uris.length == hps.length &&
+                hps.length == damages.length &&
+                damages.length == crits.length &&
+                crits.length == heals.length,
+            "One of the given default arrays is odd length"
+        );
+        for (uint256 i = 0; i < names.length; i++) {
+            defaultAttributes.push(
+                Attributes({
+                    index: i,
+                    name: names[i],
+                    uri: uris[i],
+                    hp: hps[i],
+                    maxHp: hps[i],
+                    damage: damages[i],
+                    crit: crits[i],
+                    heal: heals[i]
+                })
+            );
+        }
+        // for (uint256 i = 0; i < defaultAttributes.length; i++) {
+        //     console.logUint(defaultAttributes[i].index);
+        //     console.logString(defaultAttributes[i].name);
+        //     console.logString(defaultAttributes[i].uri);
+        //     console.logUint(defaultAttributes[i].hp);
+        //     console.logUint(defaultAttributes[i].maxHp);
+        //     console.logUint(defaultAttributes[i].damage);
+        //     console.logUint(defaultAttributes[i].crit);
+        //     console.logUint(defaultAttributes[i].heal);
+        // }
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,18 @@
 const main = async () => {
 	const gameContractFactory = await hre.ethers.getContractFactory("Game");
-	const gameContract = await gameContractFactory.deploy();
+	const gameContract = await gameContractFactory.deploy(
+		["Warrior", "Thief", "Druid"], // Names
+		[
+			// Image URIs
+			"https://ibb.co/hmj35qJ",
+			"https://ibb.co/FKW4C4X",
+			"https://ibb.co/tQfbVfH",
+		],
+		[100, 60, 40], // HPs
+		[10, 5, 2], // Damages
+		[20, 50, 10], // Crit chances
+		[0, 0, 10] // Heal
+	);
 	await gameContract.deployed();
 	console.log("Contract deployed to:", gameContract.address);
 };
