2a911558b3b2d68e5c3933044dc0bc868e33b978
Santeri Suomi <sant.suomi@gmail.com>
2022-04-30 21:48:32 +0300

    Frontend: add basic landing page with a connect button.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,105 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import { ethers } from "ethers";
 
-function App() {
+export default function App() {
+	const [account, setAccount] = useState(null);
+
+	const { ethereum } = window;
+
+	const connectWallet = async () => {
+		try {
+			if (!ethereum) {
+				alert("hj");
+			}
+
+			const provider = new ethers.providers.Web3Provider(ethereum);
+			await provider.send("eth_requestAccounts", []);
+			const signer = provider.getSigner();
+
+			setAccount({
+				provider: provider,
+				accounts: signer,
+			});
+			console.log(
+				(await provider.getBalance(signer.getAddress())).toString()
+			);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		const checkWalletConnection = async () => {
+			try {
+				if (ethereum) {
+					console.log("Ethereum object found", ethereum);
+				} else {
+					console.log("Make sure you have MetaMask!");
+				}
+
+				const accounts = await ethereum.request({
+					method: "eth_accounts",
+				});
+
+				if (accounts.length === 0) {
+					console.log("No authorized accounts");
+				} else {
+					const provider = new ethers.providers.Web3Provider(
+						ethereum
+					);
+					const signer = provider.getSigner();
+					setAccount({
+						provider: provider,
+						accounts: signer,
+					});
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		if (!account) {
+			checkWalletConnection();
+		}
+	}, [account, ethereum]);
+
 	return (
-		<div className="App">
-			<h1>Hello!</h1>
-			<p>Something is brewing here. 🙈</p>
-		</div>
+		<Stack direction="column" spacing={7.5}>
+			<Stack
+				direction="row"
+				justifyContent="space-between"
+				alignItems="center"
+			>
+				<Typography fontSize={35}>👾 Monster Slayer 👹</Typography>
+				<Button
+					sx={{
+						maxHeight: 45,
+						backgroundColor: "#6e2c90",
+					}}
+					variant="contained"
+					onClick={() => {
+						connectWallet();
+					}}
+				>
+					{account ? "Connected" : "Connect"}
+				</Button>
+			</Stack>
+			<Stack direction="column" alignItems="center" spacing={3}>
+				<Box
+					component="img"
+					sx={{
+						height: 250,
+						width: 375,
+					}}
+					alt="The house from the offer."
+					src="main.webp"
+				/>
+				<Typography fontSize={18.5}>
+					Connect your wallet to get started (top right)!
+				</Typography>
+			</Stack>
+		</Stack>
 	);
 }
-
-export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,22 @@
+import { ThemeProvider } from "@mui/system";
+import { createTheme } from "@mui/material/styles";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+const theme = createTheme({
+	typography: {
+		allVariants: {
+			color: "#ffffff",
+		},
+	},
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 );
