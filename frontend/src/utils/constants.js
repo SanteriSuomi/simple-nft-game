177f7ca2a76e21216ffabbb3ed6ad0d4e357e6c7
Santeri Suomi <sant.suomi@gmail.com>
2022-05-07 23:46:58 +0300

    Add functions to automatically copy ABI and contract address to frontend public folder

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,7 @@
+const CONTRACT_ADDRESS = "0x413b1AfCa96a3df5A686d8BFBF93d30688a7f7D9";
+
+function getContractAddress() {
+	// TODO
+}
+
+export { CONTRACT_ADDRESS };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,44 @@
 const { initializeGameContract } = require("../utils/Utilities");
+const fs = require("fs");
+const path = require("path");
 
 const main = async () => {
-	await initializeGameContract(true, hre.network.name, "0.01");
+	const contract = await initializeGameContract(
+		true,
+		hre.network.name,
+		"0.01"
+	);
+
+	// Project root
+	const rootPath = path.join(path.dirname(require.main.filename), "../");
+
+	function getFromPath(paths, fileName) {
+		return path.resolve(rootPath, ...paths, fileName);
+	}
+
+	function getToPath(fileName) {
+		return path.resolve(rootPath, "frontend", "public", fileName);
+	}
+
+	function copyAbiToFrontend() {
+		const abiName = "Game.json";
+		fs.copyFileSync(
+			getFromPath(["artifacts", "contracts", "Game.sol"], abiName),
+			getToPath(abiName)
+		);
+		console.log("Updated ABI in frontend");
+	}
+
+	function copyAddressToFrontend() {
+		const addressFileName = "Address";
+		fs.writeFileSync(getToPath(addressFileName), contract.address, {
+			flag: "w+",
+		});
+		console.log("Updated contract address in frontend");
+	}
+
+	copyAbiToFrontend();
+	copyAddressToFrontend();
 };
 
 main()
